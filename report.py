81bf051eb13799caa7f4d90aa8d7f819ebe2b37b
yasiryagi <yasir.yagi@gmail.com>
2022-06-30 00:35:26 +0200

    nothing should change

--- report.py
+++ report.py
@@ -365,7 +365,7 @@ if __name__ == '__main__':
   print('## Objects Size Distribution')
   report += '## Objects Size Distribution \n'
   tble = print_table([sizes])
-  report += tble+'\n'
+  report += tble+'\n\n'
   print('\n')
 
   tble = print_table([sizes_range])
