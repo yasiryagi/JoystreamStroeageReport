3278871e4e469a4f263db9f810897ff8c83aef8f
yasiryagi <yasir.yagi@gmail.com>
2022-07-04 12:15:45 +0200

    plot objects

--- report.py
+++ report.py
@@ -293,8 +293,8 @@ def get_draw_objects(file1name, file2name):
   plot(dates, num_objects, 'Number of Objects', 'Dates', 'Number of Objects', 10, 1500,file2name)
 
 def plot(x, y, title, x_label, y_label, x_spacing, y_spacing,filename):
-  plt.rcParams["figure.figsize"] = (10,8)
   fig, ax = plt.subplots()
+  fig.set_size_inches(15, 10)
   plt.plot(x, y)
   ax.set_xticks(np.arange(0, len(x)+1, x_spacing))
   ax.set_yticks(np.arange(0, max(y), y_spacing))
@@ -302,8 +302,8 @@ def plot(x, y, title, x_label, y_label, x_spacing, y_spacing,filename):
   ax.set(xlabel=x_label, ylabel=y_label)
   plt.xticks(rotation=45)
   plt.yticks(rotation=45)
-
-  plt.savefig(filename)
+  fig.set_dpi(100)
+  fig.savefig(filename)
   plt.close()
   
 
