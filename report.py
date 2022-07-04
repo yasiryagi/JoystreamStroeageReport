aea556c0158c361fb735a4c560d0db246e243fdf
yasiryagi <yasir.yagi@gmail.com>
2022-07-04 12:05:43 +0200

    plot objects

--- report.py
+++ report.py
@@ -294,6 +294,7 @@ def get_draw_objects(file1name, file2name):
 
 def plot(x, y, title, x_label, y_label, x_spacing, y_spacing,filename):
   fig, ax = plt.subplots()
+  plt.rcParams["figure.figsize"] = (10,8)
   plt.plot(x, y)
   ax.set_xticks(np.arange(0, len(x)+1, x_spacing))
   ax.set_yticks(np.arange(0, max(y), y_spacing))
