24233d149730beaad4d4a129cbf229979a43cb76
yasiryagi <yasir.yagi@gmail.com>
2022-07-04 12:31:52 +0200

    plot objects

--- report.py
+++ report.py
@@ -289,8 +289,8 @@ def get_draw_objects(file1name, file2name):
     num_objects[index] += num_objects[index-1]  
   
 
-  plot(dates[1:], sizes[1:], 'Size (Sum, GB)', 'Dates', 'Size', 0, 700 , 10, 100,file1name)
-  plot(dates[1:], num_objects[1:], 'Number of Objects', 'Dates', 'Number of Objects', 0, 1200, 10, 1500,file2name)
+  plot(dates[1:], sizes[1:], 'Size (Sum, GB)', 'Dates', 'Size', 0, 750 , 10, 100,file1name)
+  plot(dates[1:], num_objects[1:], 'Number of Objects', 'Dates', 'Number of Objects', 0, 12000, 10, 1500,file2name)
 
 def plot(x, y, title, x_label, y_label, x_start, y_start, x_spacing, y_spacing,filename):
   fig, ax = plt.subplots()
